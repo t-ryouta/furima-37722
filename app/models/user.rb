4219f38b07a3cd1ed3cd6342e6eca7a989917cb2
t-ryouta <r.i.p.terry3.respect@gmail.com>
2022-04-04 12:29:59 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :nickname, precence: true
+  validates :first_name, precence: true
+  validates :last_name, precence: true
+  validates :first_name_kana, precence: true
+  validates :last_name_kana, precence: true
+  validates :birthday, precence: true
 end
