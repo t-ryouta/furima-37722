6fb11cdd10f8fcfe0de640e7e47250b344b05cda
t-ryouta <r.i.p.terry3.respect@gmail.com>
2022-04-05 20:55:20 +0900

    テストコード　「passwordは半角英数字を含まないと登録できない」を３つの条件に分割

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -45,9 +45,17 @@ RSpec.describe User, type: :model do
         @user.valid?
         expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
       end
-      it 'passwordは半角英数字を含まないと登録できない' do
+      it 'passwordは半角数字のみでは登録できない' do
         @user.password = '000000'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password is invalid. Include both letters and numbers')
+      end
+      it 'passwordは半角英字のみでは登録できない' do
         @user.password = 'abcdef'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password is invalid. Include both letters and numbers')
+      end
+      it 'passwordは全角文字を含むパスワードでは登録できない' do
         @user.password = '００００００'
         @user.valid?
         expect(@user.errors.full_messages).to include('Password is invalid. Include both letters and numbers')
