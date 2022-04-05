fcfca4d90611f113ddd7c3cd14a343b22bb1035f
t-ryouta <r.i.p.terry3.respect@gmail.com>
2022-04-05 20:14:32 +0900

    user.rb　first_nameとlast_nameのバリデーションを修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   validates :nickname, presence: true
   validates :birthday, presence: true
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'is invalid. Input full-width characters' } do
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters' } do
     validates :first_name
     validates :last_name
   end
