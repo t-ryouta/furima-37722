21ede6ba78ab54b9b3937cd8773f0c7604a5d0e9
t-ryouta <r.i.p.terry3.respect@gmail.com>
2022-04-05 19:30:59 +0900

    Rubocopを実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,8 +13,7 @@ class User < ApplicationRecord
     validates :first_name_kana
     validates :last_name_kana
   end
-  
 
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX, message: 'is invalid. Include both letters and numbers' 
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'is invalid. Include both letters and numbers'
 end
