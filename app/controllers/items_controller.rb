26a2206df27eab77a2b0923fe235144580eed7b2
t-ryouta <r.i.p.terry3.respect@gmail.com>
2022-04-07 16:36:15 +0900

    出品商品をデータベースに保存

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,11 +8,16 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
 
   def item_params
-    params.require(:item).permit(:goods, :catch_copy, :category_id, :condition_id, :burden_id, :area_id, :delivery_day_id, :price, :image)
+    params.require(:item).permit(:goods, :catch_copy, :category_id, :condition_id, :burden_id, :area_id, :delivery_day_id, :price, :image).merge(user_id: current_user.id)
   end
 end
