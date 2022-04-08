120b30a1d83f0f747b848577dfe8a56dc364f60c
t-ryouta <r.i.p.terry3.respect@gmail.com>
2022-04-08 19:11:42 +0900

    priceのバリデーションにonly_integer: trueを追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -17,7 +17,7 @@ class Item < ApplicationRecord
   validates :area_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
   validates :delivery_day_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
   with_options presence: true, format: { with: /\A[0-9]+\z/, message: 'is invalid. Input half-width characters' } do
-    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 , message: 'is out of setting range'}
+    validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 , message: 'is out of setting range'}
   end
   validates :image, presence: true
 end
