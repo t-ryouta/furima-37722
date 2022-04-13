79a6b4635c2d7aa3ce4f0df9f933aedc6ee8f4cc
t-ryouta <r.i.p.terry3.respect@gmail.com>
2022-04-13 17:31:43 +0900

    deliveriesテーブルを作成

--- db/migrate/20220413074137_create_deliveries.rb
+++ db/migrate/20220413074137_create_deliveries.rb
@@ -1,7 +1,13 @@
 class CreateDeliveries < ActiveRecord::Migration[6.0]
   def change
     create_table :deliveries do |t|
-
+      t.string     :post_code,    null: false
+      t.integer    :area_id,      null: false
+      t.string     :city,         null: false
+      t.string     :address,      null: false
+      t.string     :building_name
+      t.string     :phone_number, null: false
+      t.references :order,    null: false, foreign_key: true
       t.timestamps
     end
   end
