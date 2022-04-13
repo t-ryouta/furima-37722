7645a0a149a69e94add2b9bf202aa677364966ac
t-ryouta <r.i.p.terry3.respect@gmail.com>
2022-04-13 16:24:21 +0900

    バリデーションを設定

--- app/models/order_delivery.rb
+++ app/models/order_delivery.rb
@@ -1,4 +1,16 @@
 class OrderDelivery
   include ActiveModel::Model
   attr_accessour :user_id, :item_id, :post_code, :area_id, :city, :address, :building_name, :phone_number, :user_item
+  with_options presence: true do
+    validates :user_id
+    validates :item_id
+    validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)"}
+    validates :area_id, numericality: {other_than: 0, message: "can't be blank"}
+    validates :city
+    validates :address
+    validates :user_item
+  end
+   with_options presence: true, format {with: /\A\d{10,11}\z/, message: "is too short" do
+    validates :phone_number, numericality: {only_integer: true, message: "is invalid. Input only number"}
+  end
 end
