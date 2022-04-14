d040b6254b53862d947580fd211d929d96312bd7
t-ryouta <r.i.p.terry3.respect@gmail.com>
2022-04-14 16:40:21 +0900

    配送先情報のテストコードを記述

---
+++ spec/factories/order_deliveries.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :order_delivery do
+    post_code { '123-4567' }
+    area_id { '2' }
+    city { '東京都' }
+    address { '1-1' }
+    building_name { '東京ハイツ' }
+    phone_number { '09012345678' }
+  end
+end
