514a8b0e17884fca24720f8af9bfb10b2174e56b
t-ryouta <r.i.p.terry3.respect@gmail.com>
2022-04-14 12:29:15 +0900

    商品出品時に登録した情報を表示する記述を修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,11 +1,11 @@
 class OrdersController < ApplicationController
   def index
     @order_delivery = OrderDelivery.new
+    @items = Item.all
+    @items = Item.find(params[:item_id])
   end
 
   def create
-    @items = Item.all
-    @items = Item.find(params[:item_id])
     @order_delivery = OrderDelivery.new(order_params)
     if @order_delivery.valid?
       @order_delivery.save
@@ -18,6 +18,6 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order_delivery).permit(:post_code, :area_id, :city, :address, :building_name, :phone_number, :order).merge(user_id: current_user.id)
+    params.require(:order_delivery).permit(:post_code, :area_id, :city, :address, :building_name, :phone_number, :order).merge(user_id: current_user.id, item_id: :item_id)
   end
 end
