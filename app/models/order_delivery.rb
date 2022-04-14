a2826399b9710e2af2619be2824be4d87dfa2d92
t-ryouta <r.i.p.terry3.respect@gmail.com>
2022-04-14 15:12:08 +0900

    order_deliveryモデルを修正

--- app/models/order_delivery.rb
+++ app/models/order_delivery.rb
@@ -1,6 +1,6 @@
 class OrderDelivery
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :post_code, :area_id, :city, :address, :building_name, :phone_number, :order
+  attr_accessor :user_id, :item_id, :post_code, :area_id, :city, :address, :building_name, :phone_number
   with_options presence: true do
     validates :user_id
     validates :item_id
@@ -8,16 +8,13 @@ class OrderDelivery
     validates :area_id, numericality: {other_than: 0, message: "can't be blank"}
     validates :city
     validates :address
-    validates :order
   end
    with_options presence: true, format: {with: /\A\d{10,11}\z/, message: "is too short" } do
     validates :phone_number, numericality: {only_integer: true, message: "is invalid. Input only number"}
   end
 
   def save
-    def save
-      order = Order.create(user_id: user_id, item_id: item_id)
-      Delivery.create(post_code: post_code, area_id: area_id, city: city, address: address, building_name: building_name, phone_number: phone_number, order: order)
-    end
+    order = Order.create(user_id: user_id, item_id: item_id)
+    Delivery.create(post_code: post_code, area_id: area_id, city: city, address: address, building_name: building_name, phone_number: phone_number, order_id: order.id)
   end
 end
