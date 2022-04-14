68115f3ddf5299ab45966c69ab552ba772e36af1
t-ryouta <r.i.p.terry3.respect@gmail.com>
2022-04-14 21:35:05 +0900

    クレジットカード決済実装に関する記述

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -10,6 +10,7 @@ class OrdersController < ApplicationController
     @items = Item.find(params[:item_id])
     @order_delivery = OrderDelivery.new(order_params)
     if @order_delivery.valid?
+      pay_item
       @order_delivery.save
       redirect_to root_path
     else
@@ -20,6 +21,14 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order_delivery).permit(:post_code, :area_id, :city, :address, :building_name,:phone_number).merge(user_id: current_user.id, item_id: @items.id)
+    params.require(:order_delivery).permit(:post_code, :area_id, :city, :address, :building_name,:phone_number).merge(user_id: current_user.id, item_id: @items.id, token:params[:token])
+  end
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @items.price,
+      card: order_params[:token],
+      currency: 'jpy'
+    )
   end
 end
--- app/models/order_delivery.rb
+++ app/models/order_delivery.rb
@@ -1,9 +1,10 @@
 class OrderDelivery
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :post_code, :area_id, :city, :address, :building_name, :phone_number
+  attr_accessor :user_id, :item_id, :post_code, :area_id, :city, :address, :building_name, :phone_number, :token
   with_options presence: true do
     validates :user_id
     validates :item_id
+    validates :token
     validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)"}
     validates :area_id, numericality: {other_than: 1, message: "can't be blank"}
     validates :city
