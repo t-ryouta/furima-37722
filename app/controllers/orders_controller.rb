5c3293e49685d5d90854192dc54deb405dea6316
t-ryouta <r.i.p.terry3.respect@gmail.com>
2022-04-14 11:36:08 +0900

    商品出品時に登録した情報を表示

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,4 +2,22 @@ class OrdersController < ApplicationController
   def index
     @order_delivery = OrderDelivery.new
   end
+
+  def create
+    @items = Item.all
+    @items = Item.find(params[:item_id])
+    @order_delivery = OrderDelivery.new(order_params)
+    if @order_delivery.valid?
+      @order_delivery.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_delivery).permit(:post_code, :area_id, :city, :address, :building_name, :phone_number, :order).merge(user_id: current_user.id)
+  end
 end
